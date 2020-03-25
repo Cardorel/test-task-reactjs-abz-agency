c57c55a9568338b20b21c2e8e36cfb53a3bad2d4
Ngassaki <cardorelngassaki@gmail.com>
2020-03-25 15:16:53 +0200

    some modification in HTML code and all CSS code wroted

--- src/front-end/RELATIONSHIPS/relationships.js
+++ src/front-end/RELATIONSHIPS/relationships.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import '../SCSS/__relationship.scss';
+import manLaptop from '../../Image/man-laptop-v1.svg'
 
 export default function Relationships() {
     return (
-        <section>
+        <section id="Relationship">
             <div className="container__relation__ship">
                 <h1>Let's get acquainted</h1>
                 <div className="Acquainted__container__flex">
                     <div className="image__svg__container">
-                        <img src="" alt=""/>
+                        <img src={manLaptop} alt={'manlaptop'}/>
                     </div>
                     <div className="relationship__requirement__containers">
                         <h2>I am cool frontend developper</h2>
