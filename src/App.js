d8d7aeee65fc48acd8d1ad9885578004912cc237
Ngassaki <cardorelngassaki@gmail.com>
2020-03-27 01:45:20 +0200

    create css and html for footer.finished with both

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AboutMe from './front-end/ABOUTME/aboutMe'
 import Relationships from './front-end/RELATIONSHIPS/relationships'
 import Users from './front-end/USERS/users'
 import Signup from './front-end/SIGNUP/signup'
+import Footer from './front-end/FOOTER/footer';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Relationships/>
       <Users/>
       <Signup/>
+      <Footer/>
     </div>
   );
 }
