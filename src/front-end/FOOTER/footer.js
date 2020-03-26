0b51c265cbc1a8008a1dca5ac881ad9e898116e0
Ngassaki <cardorelngassaki@gmail.com>
2020-03-27 01:47:51 +0200

    create css and html for modal

---
+++ src/front-end/FOOTER/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../SCSS/__footer.scss';
+
+export default function Footer() {
+    return (
+        <>
+           <div className="footer__container">
+             <hr/>
+             <p>© abz.agency specially for the test task</p>
+           </div>
+        </>
+    )
+}
