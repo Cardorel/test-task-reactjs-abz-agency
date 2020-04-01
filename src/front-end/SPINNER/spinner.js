744c58a852fed1881dff0344c4a1427552051dee
Ngassaki <cardorelngassaki@gmail.com>
2020-04-01 04:15:56 +0300

    Creation of spinner on use the svg + scss

---
+++ src/front-end/SPINNER/spinner.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../SCSS/__spinner.scss"
+
+export default function Spinner() {
+    return (
+        <svg className="spinner" viewBox="0 0 50 50">
+            <circle className="path" cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>
+        </svg>
+    )
+}
