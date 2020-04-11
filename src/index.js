f739896d3cf85f962397cb07dc581b9dfcecdd1d
Ngassaki <cardorelngassaki@gmail.com>
2020-04-11 18:26:15 +0300

    Yraaaaa!

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { CombineReducers } from './backend/REDUX/COMBINE/combineReducer'
+import { CombineReducers } from './backend/REDUX/COMBINE/combineReducer';
 
 const middleware = [thunk];
 
